0b4059477d4e628a9b3d59833e780d22716be879
Christa DeJesus <indychrista@gmail.com>
2022-08-13 17:31:12 -0400

    Change CNAME location to public folder, add https to homepage, add basename to browserrouter in index.js, all to fix blank page on deploy

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import ScrollToTop from "./components/utility/ScrollToTop";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <ScrollToTop />
       <App />
     </BrowserRouter>
